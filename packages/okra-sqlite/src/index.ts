a0d7fd01f7dfe3e0caae516da825500cb3057397
Joel Gustafson <joelg@mit.edu>
2024-01-31 13:39:18 -0800

    packages/okra-sqlite: add tests

---
+++ packages/okra-sqlite/src/index.ts
@@ -0,0 +1,251 @@
+import type * as sqlite from "better-sqlite3"
+import Database from "better-sqlite3"
+
+import { blake3 } from "@noble/hashes/blake3"
+import { Key, Node, lessThan } from "@canvas-js/okra"
+import { bytesToHex } from "@noble/hashes/utils"
+
+type NodeRecord = { level: number; key: Uint8Array | null; hash: Uint8Array; value: Uint8Array | null }
+
+export class Tree {
+	private readonly K: number
+	private readonly Q: number
+	private readonly LIMIT: number
+	private readonly LEAF_ANCHOR_HASH: Uint8Array
+
+	private readonly db: sqlite.Database
+	private readonly statements: {
+		insert: sqlite.Statement
+		update: sqlite.Statement
+		delete: sqlite.Statement
+		select: sqlite.Statement
+		selectRange: sqlite.Statement
+		selectRangeReverse: sqlite.Statement
+		selectRoot: sqlite.Statement
+	}
+
+	constructor(path: string | null = null, options: { K?: number; Q?: number } = {}) {
+		this.K = options.K ?? 16
+		this.Q = options.Q ?? 32
+		this.LIMIT = Number((1n << 32n) / BigInt(this.Q))
+		this.LEAF_ANCHOR_HASH = blake3(new Uint8Array([]), { dkLen: this.K })
+
+		this.db = new Database(path ?? ":memory:")
+
+		this.db.exec(`CREATE TABLE IF NOT EXISTS nodes (level INTEGER NOT NULL, key BLOB, hash BLOB NOT NULL, value BLOB)`)
+
+		this.db.exec(`CREATE UNIQUE INDEX IF NOT EXISTS node_index ON nodes(level, key)`)
+
+		this.statements = {
+			insert: this.db.prepare(`INSERT INTO nodes VALUES (:level, :key, :hash, :value)`),
+			update: this.db.prepare(
+				`UPDATE nodes SET hash = :hash, value = :value WHERE level = :level AND ((key ISNULL AND :key ISNULL) OR (key = :key))`
+			),
+			delete: this.db.prepare(
+				`DELETE FROM nodes WHERE level = :level AND ((key ISNULL AND :key ISNULL) OR (key = :key))`
+			),
+			select: this.db.prepare(
+				`SELECT * FROM nodes WHERE level = :level AND ((key ISNULL AND :key ISNULL) OR (key = :key))`
+			),
+			selectRange: this.db.prepare(
+				`SELECT * FROM nodes WHERE level = :level AND (:key ISNULL OR key >= :key) ORDER BY key ASC`
+			),
+			selectRangeReverse: this.db.prepare(
+				`SELECT * FROM nodes WHERE level = :level AND (key ISNULL OR key <= :key) ORDER BY key DESC`
+			),
+			selectRoot: this.db.prepare(`SELECT * FROM nodes ORDER BY level DESC LIMIT 1`),
+		}
+
+		this.setNode({ level: 0, key: null, hash: this.LEAF_ANCHOR_HASH })
+	}
+
+	public getRoot(): Node {
+		const { level, key, hash } = this.statements.selectRoot.get() as NodeRecord
+		return { level, key, hash }
+	}
+
+	public set(key: Uint8Array, value: Uint8Array) {
+		const oldLeaf = this.getNode(0, key)
+		const hash = this.hashEntry(key, value)
+		const newLeaf: Node = { level: 0, key, hash, value }
+
+		this.replace(oldLeaf, newLeaf)
+	}
+
+	public delete(key: Uint8Array) {
+		const node = this.getNode(0, key)
+		if (node === null) {
+			return
+		}
+
+		if (node.key !== null && this.isBoundary(node)) {
+			this.deleteParents(0, key)
+		}
+
+		this.deleteNode(0, key)
+
+		const firstSibling = this.getFirstSibling(node)
+		if (firstSibling.key === null) {
+			this.updateAnchor(1)
+		} else {
+			this.update(1, firstSibling.key)
+		}
+	}
+
+	private update(level: number, key: Key) {
+		const oldNode = this.getNode(level, key)
+		const hash = this.getHash(level, key)
+		const newNode: Node = { level, key, hash }
+		this.replace(oldNode, newNode)
+	}
+
+	private replace(oldNode: Node | null, newNode: Node) {
+		if (oldNode !== null && this.isBoundary(oldNode)) {
+			this.replaceBoundary(newNode)
+		} else {
+			const firstSibling = this.getFirstSibling(newNode)
+
+			this.setNode(newNode)
+			if (this.isBoundary(newNode)) {
+				this.createParents(newNode.level, newNode.key)
+			}
+
+			if (firstSibling.key == null) {
+				this.updateAnchor(newNode.level + 1)
+			} else {
+				this.update(newNode.level + 1, firstSibling.key)
+			}
+		}
+	}
+
+	private replaceBoundary(node: Node) {
+		if (this.isBoundary(node)) {
+			this.setNode(node)
+			this.update(node.level + 1, node.key)
+		} else {
+			this.setNode(node)
+			this.deleteParents(node.level, node.key)
+
+			const firstSibling = this.getFirstSibling(node)
+			if (firstSibling.key === null) {
+				this.updateAnchor(node.level + 1)
+			} else {
+				this.update(node.level + 1, firstSibling.key)
+			}
+		}
+	}
+
+	private async updateAnchor(level: number) {
+		const hash = this.getHash(level, null)
+		this.setNode({ level, key: null, hash })
+
+		let terminate = true
+		for (const row of this.statements.selectRange.iterate({ level, key: null })) {
+			const node = row as Node
+			if (node.key === null) {
+				continue
+			} else {
+				terminate = false
+				break
+			}
+		}
+
+		if (terminate) {
+			this.deleteParents(level, null)
+		} else {
+			this.updateAnchor(level + 1)
+		}
+	}
+
+	private deleteParents(level: number, key: Key) {
+		const node = this.getNode(level + 1, key)
+		if (node !== null) {
+			this.deleteNode(level + 1, key)
+			this.deleteParents(level + 1, key)
+		}
+	}
+
+	private createParents(level: number, key: Key) {
+		const hash = this.getHash(level + 1, key)
+		const node: Node = { level: level + 1, key, hash }
+		this.setNode(node)
+		if (this.isBoundary(node)) {
+			this.createParents(level + 1, key)
+		}
+	}
+
+	private getFirstSibling(node: Node): Node {
+		if (node.key === null) {
+			return node
+		}
+
+		for (const row of this.statements.selectRangeReverse.iterate({ level: node.level, key: node.key })) {
+			const node = row as Node
+			if (node.key === null || this.isBoundary(node)) {
+				return node
+			}
+		}
+
+		throw new Error("Internal error")
+	}
+
+	private getHash(level: number, key: Key): Uint8Array {
+		const hash = blake3.create({ dkLen: this.K })
+
+		for (const row of this.statements.selectRange.iterate({ level: level - 1, key })) {
+			const node = row as Node
+			if (lessThan(key, node.key) && this.isBoundary(node)) {
+				break
+			}
+
+			hash.update(node.hash)
+		}
+
+		return hash.digest()
+	}
+
+	private getNode(level: number, key: Key): Node | null {
+		const row = this.statements.select.get({ level, key }) as NodeRecord | undefined
+		if (row === undefined) {
+			return null
+		}
+
+		const { hash, value } = row
+		if (value === null) {
+			return { level, key, hash }
+		} else {
+			return { level, key, hash, value }
+		}
+	}
+
+	private setNode({ level, key, hash, value }: Node) {
+		if (this.getNode(level, key) === null) {
+			this.statements.insert.run({ level, key, hash, value: value ?? null })
+		} else {
+			this.statements.update.run({ level, key, hash, value: value ?? null })
+		}
+	}
+
+	private deleteNode(level: number, key: Key) {
+		this.statements.delete.run({ level, key })
+	}
+
+	private isBoundary({ hash }: Node) {
+		const view = new DataView(hash.buffer, hash.byteOffset, hash.byteLength)
+		return view.getUint32(0) < this.LIMIT
+	}
+
+	private static size = new ArrayBuffer(4)
+	private static view = new DataView(Tree.size)
+
+	private hashEntry(key: Uint8Array, value: Uint8Array): Uint8Array {
+		const hash = blake3.create({ dkLen: this.K })
+		Tree.view.setUint32(0, key.length)
+		hash.update(new Uint8Array(Tree.size))
+		hash.update(key)
+		Tree.view.setUint32(0, value.length)
+		hash.update(new Uint8Array(Tree.size))
+		hash.update(value)
+		return hash.digest()
+	}
+}
