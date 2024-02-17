2b1e346ed5319baf90e0ce374ba0c78a17034cb2
Raymond Z <raykyri@gmail.com>
2024-02-17 16:37:26 -0500

    compare trees, add getValue() test

--- packages/okra-pg/test/tree.test.ts
+++ packages/okra-pg/test/tree.test.ts
@@ -12,7 +12,7 @@ import {
 	Tree as SqliteTree,
 } from "@canvas-js/okra-sqlite"
 
-test("compare pg to sqlite(1000) with interleaved deletes", async (t) => {
+test.serial("compare pg to sqlite(1000)", async (t) => {
 	const path = "postgresql://localhost:5432/test" // TODO
 	const client = new pg.Client(path)
 
@@ -29,6 +29,35 @@ test("compare pg to sqlite(1000) with interleaved deletes", async (t) => {
 		const value = blake3(key, { dkLen: 4 })
 		await tree.set(key, value)
 		await sqliteTree.set(key, value)
+	}
+
+	const pgRoot = await tree.getRoot()
+	await client.end()
+
+	t.deepEqual(pgRoot, sqliteTree.getRoot())
+
+	t.log("YAY")
+	t.pass()
+})
+
+test.serial("compare pg to sqlite(100) with interleaved deletes", async (t) => {
+	const path = "postgresql://localhost:5432/test" // TODO
+	const client = new pg.Client(path)
+
+	const hasher = new Sha256Hasher({ size: new ArrayBuffer(4), K: 16 })
+	const tree = await Tree.initialize(client, { K: 16, Q: 4, clear: true, hasher })
+	const sqliteHasher = new SqliteSha256Hasher({ size: new ArrayBuffer(4), K: 16 })
+	const sqliteTree = new SqliteTree(null, { K: 16, Q: 4, hasher: sqliteHasher })
+
+	const buffer = new ArrayBuffer(4)
+	const view = new DataView(buffer)
+
+	for (let i = 0; i < 100; i++) {
+		view.setUint32(0, i)
+		const key = new Uint8Array(buffer, 0, 4)
+		const value = blake3(key, { dkLen: 4 })
+		await tree.set(key, value)
+		await sqliteTree.set(key, value)
 
 		if (i > 50 && i % 2 === 0) {
 			view.setUint32(0, i - 50)
@@ -39,15 +68,16 @@ test("compare pg to sqlite(1000) with interleaved deletes", async (t) => {
 		}
 	}
 
-	console.log("pg:\n")
-	console.log(await text(tree.print()))
+	for (let i = 0; i < 100; i++) {
+		view.setUint32(0, i)
+		const key = new Uint8Array(buffer, 0, 4)
+		t.deepEqual(await tree.getValue(key), sqliteTree.getValue(key))
+	}
 
-	console.log("sqlite:\n")
-	console.log(await text(sqliteTree.print()))
-
-	t.deepEqual(await tree.getRoot(), sqliteTree.getRoot())
-
+	const pgRoot = await tree.getRoot()
 	await client.end()
+
+	t.deepEqual(pgRoot, sqliteTree.getRoot())
 
 	t.log("YAY")
 	t.pass()
