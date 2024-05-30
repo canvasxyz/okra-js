c80ac2f3039e9fe4ca7a18e71a3594da57106d9d
Bob Webb <bob.wbb@gmail.com>
2024-05-30 14:39:07 +0100

    make transactions work by treating everything as 'static

--- packages/okra-wasm/src/lib.rs
+++ packages/okra-wasm/src/lib.rs
@@ -1,10 +1,10 @@
 use concread::bptree::{BptreeMap, BptreeMapReadTxn, BptreeMapWriteTxn};
-use ouroboros::self_referencing;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Store {
-    map: BptreeMap<String, String>
+    map: &'static BptreeMap<String, String>
+
 }
 
 #[wasm_bindgen]
@@ -12,67 +12,70 @@ impl Store {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Store {
         Store {
-            map: BptreeMap::new()
+            // we intentionally 'leak' the reference to the tree
+            // this means that its lifetime will be set to static and the data will live
+            // for the rest of the program's life
+            map: Box::leak(Box::new(BptreeMap::new()))
         }
     }
 
     #[wasm_bindgen]
-    pub fn create_read_transaction(self) -> ReadOnlyTransaction {
-        ReadOnlyTransaction::new(self.map, |map| map.read())
+    pub fn read(self) -> ReadOnlyTransaction {
+        ReadOnlyTransaction::new(self.map)
     }
 
     #[wasm_bindgen]
-    pub fn create_read_write_transaction(self) -> ReadWriteTransaction {
-        ReadWriteTransaction::new(self.map, |map| map.write())
+    pub fn write(&self) -> ReadWriteTransaction {
+        ReadWriteTransaction::new(self.map)
     }
 }
 
 #[wasm_bindgen]
-#[self_referencing(pub_extras)]
 pub struct ReadOnlyTransaction {
-    map: BptreeMap<String, String>,
-    #[borrows(map)]
-    #[not_covariant]
-    txn: BptreeMapReadTxn<'this, String, String>
+    txn: BptreeMapReadTxn<'static, String, String>
+}
+
+impl ReadOnlyTransaction {
+    pub fn new(map: &'static BptreeMap<String, String>) -> ReadOnlyTransaction {
+        ReadOnlyTransaction {
+            txn: map.read()
+        }
+    }
 }
 
 #[wasm_bindgen]
 impl ReadOnlyTransaction {
     pub fn get(&self, key: String) -> Option<String> {
-        self.with_txn(|txn| {
-            txn.get(&key).cloned()
-        })
+        self.txn.get(&key).cloned()
     }
 }
 
 // some duplicate code - can we use traits here?
 
 #[wasm_bindgen]
-#[self_referencing(pub_extras)]
 pub struct ReadWriteTransaction {
-    map: BptreeMap<String, String>,
-    #[borrows(mut map)]
-    #[not_covariant]
-    txn: BptreeMapWriteTxn<'this, String, String>
+    txn: BptreeMapWriteTxn<'static, String, String>
+}
+
+impl ReadWriteTransaction {
+    pub fn new(map: &'static BptreeMap<String, String>) -> ReadWriteTransaction {
+        ReadWriteTransaction {
+            txn: map.write()
+        }
+    }
 }
 
 #[wasm_bindgen]
 impl ReadWriteTransaction {
-    pub fn get(self, key: String) -> Option<String> {
-        self.with_txn(|txn| {
-            txn.get(&key).cloned()
-        })
+    pub fn get(&self, key: String) -> Option<String> {
+        self.txn.get(&key).cloned()
     }
 
-    pub fn set(mut self, key: String, value: String) {
-        self.with_txn_mut(|txn| {
-            txn.insert(key, value);
-        });
+    pub fn set(&mut self, key: String, value: String) {
+        self.txn.insert(key, value);
     }
 
-    pub fn commit(mut self) {
-        self.with_txn_mut(|txn| {
-            txn.commit();
-        });
+    pub fn commit(self) {
+        self.txn.commit();
     }
 }
