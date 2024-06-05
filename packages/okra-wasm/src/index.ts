31136f21aed501b7397dc14395cfe39ad5548443
Bob Webb <bob.wbb@gmail.com>
2024-06-05 11:31:54 +0200

    placeholder code + test in okra-wasm

--- packages/okra-wasm/src/index.ts
+++ packages/okra-wasm/src/index.ts
@@ -1,3 +1,6 @@
 import { Store } from "@canvas-js/bptree-wasm/src/index.js"
 
 // TODO: implement Okra using the Rust bptree
+export function doSomething() {
+	console.log("hello world")
+}
---
+++ packages/okra-wasm/test/tree.test.ts
@@ -0,0 +1,7 @@
+import test from "ava"
+import { doSomething } from "@canvas-js/okra-wasm"
+
+test("meaningless test to check that everything builds/runs", async (t) => {
+	doSomething()
+	t.pass()
+})
