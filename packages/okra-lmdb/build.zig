15aea493bccba7b08d74f565edf3c1a0452c4e26
Joel Gustafson <joelgustafson@protonmail.com>
2025-04-09 20:27:20 -0400

    packages/okra-lmdb: use standard optimize mode in build.zig

--- packages/okra-lmdb/build.zig
+++ packages/okra-lmdb/build.zig
@@ -4,23 +4,24 @@ pub fn build(b: *std.Build) void {
     const include = b.option([]const u8, "include", "Absolute path to NodeJS include directory") orelse
         "/usr/local/include/node";
 
-    addTarget(b, include, "x64-linux-glibc/lmdb.node", .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu });
-    addTarget(b, include, "x64-linux-musl/lmdb.node", .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl });
-    addTarget(b, include, "arm64-linux-glibc/lmdb.node", .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu });
-    addTarget(b, include, "arm64-linux-musl/lmdb.node", .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl });
-    addTarget(b, include, "x64-darwin/lmdb.node", .{ .cpu_arch = .x86_64, .os_tag = .macos });
-    addTarget(b, include, "arm64-darwin/lmdb.node", .{ .cpu_arch = .aarch64, .os_tag = .macos });
+    const optimize = b.standardOptimizeOption(.{});
+    addTarget(b, include, "x64-linux-glibc/lmdb.node", .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu }, optimize);
+    addTarget(b, include, "x64-linux-musl/lmdb.node", .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl }, optimize);
+    addTarget(b, include, "arm64-linux-glibc/lmdb.node", .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu }, optimize);
+    addTarget(b, include, "arm64-linux-musl/lmdb.node", .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl }, optimize);
+    addTarget(b, include, "x64-darwin/lmdb.node", .{ .cpu_arch = .x86_64, .os_tag = .macos }, optimize);
+    addTarget(b, include, "arm64-darwin/lmdb.node", .{ .cpu_arch = .aarch64, .os_tag = .macos }, optimize);
     // addTarget(b, "x64-win32/okra.node", .{ .cpu_arch = .x86_64, .os_tag = .windows });
 }
 
-fn addTarget(b: *std.Build, include: []const u8, name: []const u8, target: std.Target.Query) void {
+fn addTarget(b: *std.Build, include: []const u8, name: []const u8, target: std.Target.Query, optimize: std.builtin.OptimizeMode) void {
     const lmdb = b.dependency("lmdb", .{});
 
     const lib = b.addSharedLibrary(.{
         .name = "okra-lmdb",
         .root_source_file = b.path("napi/lib.zig"),
         .target = b.resolveTargetQuery(target),
-        .optimize = .ReleaseSafe,
+        .optimize = optimize,
     });
 
     lib.linkLibC();
